49a0005ca7f96ceeb6fa3e396b61cd4d1318e1be
chrismatta <matta14_51@hotmail.com>
2022-08-09 08:21:59 -0500

    Deploy

--- src/main/java/com/example/aplicacionempleos/configuration/WebConfig.java
+++ src/main/java/com/example/aplicacionempleos/configuration/WebConfig.java
@@ -6,7 +6,6 @@ import org.springframework.context.ApplicationContext;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.ViewResolver;
-import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
 import org.springframework.web.servlet.config.annotation.ViewResolverRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
@@ -16,7 +15,6 @@ import org.thymeleaf.spring5.view.ThymeleafViewResolver;
 
 
 @Configuration
-@EnableWebMvc
 public class WebConfig implements WebMvcConfigurer {
     @Autowired
     ApplicationContext applicationContext;
