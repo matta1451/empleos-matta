88fa34170da59c66f28455469eae998ea0bc27b1
chrismatta <matta14_51@hotmail.com>
2022-08-09 23:25:41 -0500

    Deploy

--- src/main/java/com/example/aplicacionempleos/AplicacionEmpleosApplication.java
+++ src/main/java/com/example/aplicacionempleos/AplicacionEmpleosApplication.java
@@ -11,7 +11,7 @@ public class AplicacionEmpleosApplication {
     public static void main(String[] args) {
         SpringApplication.run(AplicacionEmpleosApplication.class, args);
         ClassLoaderTemplateResolver classLoaderTemplateResolver = new ClassLoaderTemplateResolver();
-        classLoaderTemplateResolver.setPrefix("templates/");
+        classLoaderTemplateResolver.setPrefix("classpath:/templates/");
         classLoaderTemplateResolver.setSuffix(".html");
         classLoaderTemplateResolver.setTemplateMode(TemplateMode.HTML);
         classLoaderTemplateResolver.setCharacterEncoding("UTF-8");
